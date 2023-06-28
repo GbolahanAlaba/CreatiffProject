39e9624496d5dc2b196224e91e384d7f6ba9ece9
GbolahanAlaba <thegbolahanalaba@gmail.com>
2023-06-28 18:05:17 +0100

    Migrate to Postgres

--- App/settings.py
+++ App/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Creatifff',
+        'USER': 'postgres',
+        'PASSWORD': 'ablegod',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
