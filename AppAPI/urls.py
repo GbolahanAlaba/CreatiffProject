d6041abaab103d0ac3dcb97c02e562c8db7db4dd
GbolahanAlaba <thegbolahanalaba@gmail.com>
2023-06-30 07:10:27 +0100

    Updated

--- AppAPI/urls.py
+++ AppAPI/urls.py
@@ -3,11 +3,11 @@ from . import views
 from knox import views as knox_views
 
 urlpatterns = [
-    path('api/subscribe/', views.Subscribers, name='subscribe'),
-    path('api/signup_user/', views.signup_user, name='signup_user'),
-    path('api/signin_user/', views.signin_user, name='signin_user'),
-    path('api/get_user/', views.get_user, name='get_user'),
-    path('api/logout/', knox_views.LogoutView.as_view()),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view()),
+    path('api/subscribe/', views.Subscribers, name='subscribe'), #subscription link
+    path('api/signup_user/', views.signup_user, name='signup_user'), #signup link
+    path('api/signin_user/', views.signin_user, name='signin_user'), #sigin link
+    path('api/get_user/', views.get_user, name='get_user'), #get user link
+    path('api/logout/', knox_views.LogoutView.as_view()), #logout link
+    path('api/logoutall/', knox_views.LogoutAllView.as_view()), #logout all link
     
 ]
